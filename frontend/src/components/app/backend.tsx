9df76026e4fcbd2fb69663e6148555b2369aa600
Niels <nielstendijke@gmail.com>
2020-09-30 21:21:58 +0200

    Add thunk

--- frontend/src/components/app/backend.tsx
+++
@@ -1,3 +0,0 @@
-import config from "../app/config"
-
-export const ROOT_ENDPOINT = 'http://'+config.serverURL + ':' + config.serverPort
--- frontend/src/components/tasks/taskcontainer.tsx
+++ frontend/src/components/tasks/taskcontainer.tsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react"
 import TaskContainerDisplay from './taskcontainerdisplay'
-import {Task, CREATE_TASK_ENDPOINT, GET_TASK_ENDPOINT, TOGGLE_COMPLETE_TASK_ENDPOINT, REMOVE_TASK_ENDPOINT} from "./task"
+import {Task} from "./task"
 import { store } from "../app/store"
-import {ROOT_ENDPOINT} from '../app/backend'
+import {ROOT_ENDPOINT} from '../app/backendapi'
 import { SET_TASKS } from "./taskslice"
 
 function TaskContainer() {
@@ -11,83 +11,8 @@ function TaskContainer() {
     const [loading, setLoading] = useState(true)
     const [isCompletedChecked, setCompleteChecked] = useState(false)
 
-    const addNewTask = (title: string, description: string, estimatedTimeMinutes: number) =>{
-        const newTask = {
-                'title': title,
-                'description': description,
-                'estimated_time_minutes': estimatedTimeMinutes,
-            }
-        const request = ROOT_ENDPOINT + CREATE_TASK_ENDPOINT
-        const requestOptions = {
-            method: 'POST',
-            body: JSON.stringify(newTask),
-            headers: {'Content-Type': 'application/json'}
-        }
-        fetch(request, requestOptions)
-            .then(()=>setLoading(true))
-            .catch(console.log)
-    }
-
-    const getTasks = () => {
-        const request = ROOT_ENDPOINT + GET_TASK_ENDPOINT
-        fetch(request)
-            .then(res => res.json())
-            .then((tasks: Task[]) => {
-                setTasks(tasks)
-                setLoading(false)
-                store.dispatch({type: SET_TASKS, payload: tasks})
-            })
-            .catch(console.log)
-    }
-
-    const toggleCompleteTask = (id: number) =>{
-        const request = ROOT_ENDPOINT + TOGGLE_COMPLETE_TASK_ENDPOINT
-        const id_object = {
-            id: id
-        }
-        const requestOptions = {
-            method: 'POST',
-            body: JSON.stringify(id_object),
-            headers: {'Content-Type': 'application/json'}
-        }
-        fetch(request, requestOptions)
-            .then((data)=>{
-                const newTasks = tasks.map((task)=> {
-                    if(task.id === id){
-                        return {
-                            ...task,
-                            is_finished: !task.is_finished
-                        }
-                    } else{
-                        return task
-                    }
-                })
-                setTasks(newTasks)
-                store.dispatch({type: SET_TASKS, payload: newTasks})
-            })
-            .catch(console.log)
-    }
-    
     const toggleCompletedFilter = () => {
         setCompleteChecked(isCompletedChecked => !isCompletedChecked)
-    }
-
-    const removeTask = (id: number) => {
-        const request = ROOT_ENDPOINT + REMOVE_TASK_ENDPOINT
-        const id_object = {
-            id: id
-        }
-        const requestOptions = {
-            method: 'POST',
-            body: JSON.stringify(id_object),
-            headers: { 'Content-Type': 'application/json' }
-        }
-        fetch(request, requestOptions)
-            .then(() => {
-                setLoading(true)
-            })
-            .catch(console.log)
-
     }
 
     useEffect(getTasks, [loading])
