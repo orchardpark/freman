c066413d78ab477899528789026b220593b78dd5
niels <nielsfreelancer7750@gmail.com>
2021-02-10 21:48:35 +0100

    Add logged component

--- frontend/src/components/logged/loggedcontainer.tsx
+++ frontend/src/components/logged/loggedcontainer.tsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../container.css"
 import config from "../app/config"
+import Logged from './logged'
 
 function LoggedContainer() {
+    /// STATE ---------------------------------
+    const [logged, setLogged] = useState<Logged[]>([])
+    const [loading, setLoading] = useState(true)
 
+    /// FUNCTIONALITY -------------------------
+    /**
+     * Gets the logged items from the backend
+     */
+    const getLogged = () => {
+        const request = 'http://'+config.serverURL+":"+config.serverPort+"/logged"
+        fetch(request)
+        .then(res => res.json())
+        .then((logged)=>{
+            setLogged(logged)
+            setLoading(true)
+        })
+        .catch(console.log)
+    }
+
+    /**
+     * Call `getLogged` upon a change in the loading state
+     */
+    useEffect(getLogged, [loading])
+
+    /**
+     * Returns the JSX of this page
+     */
     return(
         <div className='container'>
             <div>
