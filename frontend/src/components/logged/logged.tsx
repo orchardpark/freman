dafaacfdba21763102b70970ab6bd89742636564
niels <nielsfreelancer7750@gmail.com>
2021-02-10 14:49:56 +0100

    Add logged type

---
+++ frontend/src/components/logged/logged.tsx
@@ -0,0 +1,11 @@
+type Logged = {
+    id: number,
+    created_at: Date,
+    updated_at: Date,
+    last_updated_by: string,
+    application_name: string,
+    logged_time_seconds: number,
+    window_title: string
+}
+
+export default Logged
