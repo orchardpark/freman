be46d5f0ba199e6f6b314d4d9b1bc268ed92a98d
Niels <nielsfreelancer7750@gmail.com>
2021-12-09 00:05:21 +0100

    Add abstract classifier

---
+++ backend/src/classifier/classifier.py
@@ -0,0 +1,12 @@
+import abc
+from enum import Enum
+
+class Classes(Enum):
+    PRODUCTIVE_HIGH = 1
+    PRODUCTIVE_LOW = 2
+    UNPRODUCTIVE = 3
+
+class Classifier(metaclass=abc.ABCMeta):
+	@abc.abstractmethod
+	def classify(application: str, window_title: str) -> Classes:
+		pass
