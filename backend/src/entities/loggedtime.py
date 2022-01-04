974f04eeae51dc0c22a66740cd75177f19717b8b
Niels <nielsfreelancer7750@gmail.com>
2022-01-04 20:59:33 +0100

    Add class as attribute

--- backend/src/entities/loggedtime.py
+++ backend/src/entities/loggedtime.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from .entity import Entity, Base
 from marshmallow import Schema, fields
+from ..classifier.classifier import Classes
 
 # constants
 NO_TASK = -1
@@ -11,7 +12,8 @@ class LoggedTime(Entity, Base):
     application_name = Column(String)
     logged_time_seconds = Column(Integer)
     window_title = Column(String)
-    task_id = Column(Integer)
+    task_id = Column(Integer)   
+    category = Column(Integer)
 
     def __init__(self, **kwargs):
         super().__init__()
@@ -19,6 +21,7 @@ class LoggedTime(Entity, Base):
         self.logged_time_seconds = kwargs['logged_time_seconds']
         self.window_title = kwargs['window_title']
         self.task_id = NO_TASK
+        self.category = Classes.UNKNOWN
 
 
 class LoggedTimeSchema(Schema):
