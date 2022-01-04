2acce0e3d9c9cff90caeba8206b949e7a71a8ffb
Niels <nielsfreelancer7750@gmail.com>
2022-01-04 20:59:02 +0100

    Add unknown class

--- backend/src/classifier/classifier.py
+++ backend/src/classifier/classifier.py
@@ -2,9 +2,10 @@ import abc
 from enum import Enum
 
 class Classes(Enum):
-    PRODUCTIVE_HIGH = 1
-    PRODUCTIVE_LOW = 2
-    UNPRODUCTIVE = 3
+	PRODUCTIVE_HIGH = 1
+	PRODUCTIVE_LOW = 2
+	UNPRODUCTIVE = 3
+	UNKNOWN = 4
 
 class Classifier(metaclass=abc.ABCMeta):
 	@abc.abstractmethod
