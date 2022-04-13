de83a84526470a1c843b849ced0fa6cde9124d4a
Niels <nielsfreelancer7750@gmail.com>
2022-04-14 00:20:41 +0200

    Display logged time

--- frontend/src/components/report/reportcontainerdisplay.tsx
+++ frontend/src/components/report/reportcontainerdisplay.tsx
@@ -6,10 +6,14 @@ type Props = {
 	tasks: Task[]
 }
 function ReportContainerDisplay({ logged, tasks }: Props) {
+	const totalLogged = logged.reduce((total, currentValue)=>total=total+currentValue.logged_time_seconds, 0);
 	return (
 		<div className="container">
 			<div>
 				<h1>Reports</h1>
+				<div>
+					Stats: Total logged time = {totalLogged}
+				</div>
 			</div>
 		</div>
 	)
