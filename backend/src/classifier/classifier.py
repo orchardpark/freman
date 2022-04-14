f66d6b2d6383526d2a3ef61d46f9e0fef001a273
Niels <nielsfreelancer7750@gmail.com>
2022-04-14 19:10:22 +0200

    Fix style

--- backend/src/classifier/classifier.py
+++ backend/src/classifier/classifier.py
@@ -1,13 +1,15 @@
 import abc
 from enum import Enum
 
+
 class Classes(Enum):
 	PRODUCTIVE_HIGH = 1
 	PRODUCTIVE_LOW = 2
 	UNPRODUCTIVE = 3
 	UNKNOWN = 4
 
+
 class Classifier(metaclass=abc.ABCMeta):
 	@abc.abstractmethod
-	def classify(application: str, window_title: str) -> Classes:
+	def classify(self, application: str, window_title: str) -> Classes:
 		pass
