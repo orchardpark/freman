e6774e47970ea8f643818df7d33237e742da184b
Niels <nielsfreelancer7750@gmail.com>
2022-05-06 10:49:41 +0200

    Add plotly

--- frontend/src/components/report/reportcontainerdisplay.tsx
+++ frontend/src/components/report/reportcontainerdisplay.tsx
@@ -1,6 +1,8 @@
 import Logged from "../logged/logged"
 import Task from "../tasks/task"
 import Booked from "./booked"
+import React from "react";
+import Plot from 'react-plotly.js'
 
 type Props = {
 	logged: Logged[]
@@ -8,13 +10,23 @@ type Props = {
 	booked: Booked[]
 }
 function ReportContainerDisplay({ logged, tasks, booked }: Props) {
-	const totalLogged = logged.reduce((total, currentValue)=>total=total+currentValue.logged_time_seconds, 0);
+	const totalLogged = logged.reduce((total, currentValue) => total = total + currentValue.logged_time_seconds, 0);
+	const data = [
+		{
+			x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+			y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+			mode: "lines",
+		},
+	];
+	const layout = { title: "Chart Title" };
 	return (
 		<div className="container">
 			<div>
 				<h1>Reports</h1>
 				<div>
-					Stats: Total logged time = {totalLogged}
+				</div>
+				<div>
+					<Plot data={data} layout={layout}/>
 				</div>
 			</div>
 		</div>
