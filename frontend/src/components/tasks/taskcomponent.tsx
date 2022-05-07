9d7f25ffe12fe49ea8bb8584bf267fb140f62812
Niels <nielsfreelancer7750@gmail.com>
2022-05-07 22:37:24 +0200

    Add tooltip for task buttons

--- frontend/src/components/tasks/taskcomponent.tsx
+++ frontend/src/components/tasks/taskcomponent.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "./taskcomponent.css"
+import {Overlay, Tooltip} from 'react-bootstrap'
 
 type Props = {
     is_finished: boolean,
@@ -10,17 +11,33 @@ type Props = {
 
 function TaskComponent({is_finished, toggleCompleteTask, title, id}: Props){
 
+    const [showTooltip, setShowTooltip] = React.useState(false)
+    const [toolTipText, setToolTipText] = React.useState("")
     const isCompleted = is_finished
     const todoClass = (isCompleted) ? 'completed-task': 'task'
+    const target = React.useRef(null)
 
     return (
-        <div className={todoClass} >
+        <div className={todoClass} ref={target}>
+            <Overlay target={target.current} show={showTooltip} placement='right'>
+                {(props) => (
+                    <Tooltip id="overlay-tooltip" {...props}>
+                        {toolTipText}
+                    </Tooltip>
+                )}
+            </Overlay>
             <div className={'task-field-left'}>
             </div>
             <p className={'task-field-left-2'}>{title}</p>
             <button style={{ float: "right", color: "green" }} className={'fa  fa-check'}
-                onClick={e => toggleCompleteTask(id)} />
-            <button style={{ float: 'right', color: "red" }} className={'fa fa-times'} />
+                onClick={e => toggleCompleteTask(id)}
+                onMouseEnter={() => { setToolTipText("Mark task as completed"); setShowTooltip(true) }}
+                onMouseLeave={() => { setShowTooltip(false) }}
+            />
+            <button style={{ float: 'right', color: "red" }} className={'fa fa-times'}
+                onMouseEnter={() => { setToolTipText("Delete task"); setShowTooltip(true) }}
+                onMouseLeave={() => { setShowTooltip(false) }}
+            />
         </div>
     )
 }
