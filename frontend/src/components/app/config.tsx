f98b0e7a760f3f44d95ffcd14b38eae26296d4de
Niels <nielsfreelancer7750@gmail.com>
2022-05-13 14:08:57 +0200

    Detect dev and production envirnoment

--- frontend/src/components/app/config.tsx
+++ frontend/src/components/app/config.tsx
@@ -1,7 +1,21 @@
-const config = {
+const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+const dev= {
     serverURL: '127.0.0.1',
     serverPort: 5000,
     protocol: 'http'
 }
+
+const prd = {
+    serverURL: '127.0.0.1',
+    serverPort: 5000,
+    protocol: 'http'
+}
+
+const isDev = () => {
+    return development;
+}
+
+const config = isDev() ? dev : prd
 
 export default config
